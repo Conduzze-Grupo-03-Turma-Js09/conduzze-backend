60c2df8dfb693ffb06ec08cee162b9ee8f68d727
Mariana Santana <marimoon1996@outlook.com>
2025-10-02 09:50:27 -0300

    Add files via upload

---
+++ conduzze-backend-main/conduzze-backend-main/src/motorista/services/motorista.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MotoristaService {
+  create() {
+    return;
+  }
+
+  findAll() {
+    return;
+  }
+
+  findById() {
+    return;
+  }
+  findByNomw() {
+    return;
+  }
+
+  update() {
+    return;
+  }
+
+  delete() {
+    return;
+  }
+}
