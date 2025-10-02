9afe33b0585326343e56fb3ff700f1582f575137
Mariana Santana <marimoon1996@outlook.com>
2025-10-02 11:28:14 -0300

    Update motorista.entity.ts

--- src/motorista/entities/motorista.entity.ts
+++ src/motorista/entities/motorista.entity.ts
@@ -1,18 +1,23 @@
-export class Motorista {
+import { Module } from '@nestjs/common';
+import { MotoristaController } from './controllers/corrida.controller';
+import { Motorista } from './entities/corrida.entity';
+import { MotoristaService } from './services/corrida.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Entity({ name: 'tb_motorista' })
+export class Corrida {
+  @PrimaryGeneratedColumn()
+=======
+export class Corrida {
+>>>>>>> 5882d69942159b8cd6727a2c45afeb435711965d
   id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
   nome: string;
-  avaliacao: number;
 
-  constructor(id: number, nome: string, avaliacao: number) {
-    this.id = id;
-    this.nome = nome;
-    this.avaliacao = avaliacao;
-  }
+  @IsNotEmpty()
+  @Column({ type: 'date', nullable: false })
+  avaliaçao: Number;
 
-  exibirDetalhes(): void {
-    console.log(`--- Motorista ---`);
-    console.log(`ID: ${this.id}`);
-    console.log(`Nome: ${this.nome}`);
-    console.log(`Avaliação: ${this.avaliacao}`);
-  }
 }
