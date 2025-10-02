87285a6cdf865231a716ff05300c35a8b1c537ac
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-10-02 09:23:12 -0300

    first commit

---
+++ src/usuario/services/usuario.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsuarioService {
+  create() {
+    return;
+  }
+
+  findAll() {
+    return;
+  }
+
+  findById() {
+    return;
+  }
+  findByUsuario() {
+    return;
+  }
+
+  update() {
+    return;
+  }
+}
